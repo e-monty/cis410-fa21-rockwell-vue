0a7e857fcbad92a7abe61a3d568bc0a75efffa2d
Eric <eric.montgomery@colostate.edu>
2021-12-04 12:14:30 -0700

    15.1 12/4 Vuex, login

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from "./App.vue";
 import axios from "axios";
 
 import myRouter from "./routes.js";
+import theStore from "./store.js";
 
 axios.defaults.baseURL =
   "https://cis410-fa21-montgomery-api.azurewebsites.net/";
 
 const myApp = createApp(App);
 myApp.use(myRouter);
+myApp.use(theStore);
 myApp.mount("#app");
